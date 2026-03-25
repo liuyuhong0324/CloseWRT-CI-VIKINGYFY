87928d29ee30b688259a6b65aa6e277028ee91fc
Yuzhii0718 <yuzhii0718@outlook.com>
2026-03-25 20:04:48 +0800

    项目调整

--- README.md
+++ README.md
@@ -1,32 +1,25 @@
 # CloseWRT-CI
 
-PADAVANONLY-24.10源码：
+immortalwrt-mt798x-24.10源码：
 
-https://github.com/padavanonly/immortalwrt-mt798x-24.10.git
+<https://github.com/Yuzhii0718/immortalwrt-mt798x-6.6-padavanonly.git>
 
-# U-BOOT
+## 注意
 
-闭源固件UBOOT：
-
-https://github.com/VIKINGYFY/UBOOT-CI/releases
-
-# 注意
+Yuzhii 自用。Modify for personal use only.
 
 本项目仅支持编译带有defconfig目录的MTK SDK闭源项目。
 
-# 固件简要说明
+## 固件简要说明
 
 固件每天早上4点自动编译。
 
 固件信息里的时间为编译开始的时间，方便核对上游源码提交时间。
 
-# 目录简要说明
+## 目录简要说明
 
 workflows——自定义CI配置
 
 Scripts——自定义脚本
 
 Config——自定义配置
-
-#
-[![Stargazers over time](https://starchart.cc/VIKINGYFY/CloseWRT-CI.svg?variant=adaptive)](https://starchart.cc/VIKINGYFY/CloseWRT-CI)
---
+++ Scripts/Disk.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#安装和更新软件包
+UPDATE_PACKAGE() {
+	local PKG_NAME=$1
+	local PKG_REPO=$2
+	local PKG_BRANCH=$3
+	local PKG_SPECIAL=$4
+	local PKG_LIST=("$PKG_NAME" $5)  # 第5个参数为自定义名称列表
+	local REPO_NAME=${PKG_REPO#*/}
+
+	echo " "
+
+	# 删除本地可能存在的不同名称的软件包
+	for NAME in "${PKG_LIST[@]}"; do
+		# 查找匹配的目录
+		echo "Search directory: $NAME"
+		local FOUND_DIRS=$(find ../feeds/luci/ ../feeds/packages/ -maxdepth 3 -type d -iname "*$NAME*" 2>/dev/null)
+
+		# 删除找到的目录
+		if [ -n "$FOUND_DIRS" ]; then
+			while read -r DIR; do
+				rm -rf "$DIR"
+				echo "Delete directory: $DIR"
+			done <<< "$FOUND_DIRS"
+		else
+			echo "Not fonud directory: $NAME"
+		fi
+	done
+
+	# 克隆 GitHub 仓库
+	git clone --depth=1 --single-branch --branch $PKG_BRANCH "https://github.com/$PKG_REPO.git"
+
+	# 处理克隆的仓库
+	if [[ "$PKG_SPECIAL" == "pkg" ]]; then
+		find ./$REPO_NAME/*/ -maxdepth 3 -type d -iname "*$PKG_NAME*" -prune -exec cp -rf {} ./ \;
+		rm -rf ./$REPO_NAME/
+	elif [[ "$PKG_SPECIAL" == "name" ]]; then
+		mv -f $REPO_NAME $PKG_NAME
+	fi
+}
+
+UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
+UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"
+UPDATE_PACKAGE "partexp" "sirpdboy/luci-app-partexp" "main"
+UPDATE_PACKAGE "qbittorrent" "sbwml/luci-app-qbittorrent" "master" "" "qt6base qt6tools rblibtorrent"
+UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
+UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
